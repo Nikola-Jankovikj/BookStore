e5c8c0a9e536665db7563b944a665492b3338dfb
Tea Minova <tea.minova11@gmail.com>
2024-07-02 00:35:56 +0200

    Add Export All Orders By User feature

--- BookStore.Web/Controllers/OrdersController.cs
+++ BookStore.Web/Controllers/OrdersController.cs
@@ -1,6 +1,7 @@
 ﻿using BookStore.Domain.Domain;
 using BookStore.Service.Implementation;
 using BookStore.Service.Interface;
+using ClosedXML.Excel;
 using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -63,5 +64,46 @@ namespace BookStore.Web.Controllers
             return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportInvoice.pdf");
 
         }
+
+        [HttpGet]
+        public FileContentResult ExportAllOrders()
+        {
+            string fileName = "AllOrders.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using (var workBook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workBook.Worksheets.Add("Orders");
+
+                worksheet.Cell(1, 1).Value = "Order ID";
+                worksheet.Cell(1, 2).Value = "Owner";
+
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var data = _orderService.GetAllOrdersByUser(userId);
+
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    var order = data[i];
+                    worksheet.Cell(i + 2, 1).Value = order.Id.ToString();
+                    worksheet.Cell(i + 2, 2).Value = order.Owner.Email;
+
+                    for (int j = 0; j < order.BooksInOrder.Count(); j++)
+                    {
+                        worksheet.Cell(1, j + 3).Value = "Book " + (j + 1);
+                        worksheet.Cell(i + 2, j + 3).Value = "\"" + order.BooksInOrder.ElementAt(j).Book.Title + "\" by " + order.BooksInOrder.ElementAt(j).Book.Author.Name;
+
+                    }
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, contentType, fileName);
+                }
+            }
+
+        }
+
     }
 }
