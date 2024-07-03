452b404e4d412477d3c02bd866ea389576a22eca
Tea Minova <tea.minova11@gmail.com>
2024-07-03 15:11:32 +0200

    Finish style for all Views

--- BookStore.Web/Controllers/OrdersController.cs
+++ BookStore.Web/Controllers/OrdersController.cs
@@ -54,10 +54,10 @@ namespace BookStore.Web.Controllers
             var total = 0.0;
             foreach (var item in order.Books)
             {
-                sb.AppendLine("\"" + item.Book.Title + "\" by " + item.Book.Author.Name + " - Quantity: " + item.Quantity + " - Price: " + item.Book.Price + "$");
+                sb.AppendLine("\"" + item.Book.Title + "\" by " + item.Book.Author.Name + " - Quantity: " + item.Quantity + " - Price: $" + item.Book.Price);
                 total += (item.Quantity * item.Book.Price);
             }
-            document.Content.Replace("{{OrderTotalPrice}}", total.ToString() + "$");
+            document.Content.Replace("{{OrderTotalPrice}}", "$" + total.ToString());
             document.Content.Replace("{{BooksInOrder}}", sb.ToString());
 
             var stream = new MemoryStream();
@@ -76,9 +76,10 @@ namespace BookStore.Web.Controllers
             {
                 IXLWorksheet worksheet = workBook.Worksheets.Add("Orders");
 
-                worksheet.Cell(1, 1).Value = "Order ID";
-                worksheet.Cell(1, 2).Value = "Time";
-                worksheet.Cell(1, 3).Value = "Owner";
+                worksheet.Cell(1, 1).Value = "No.";
+                worksheet.Cell(1, 2).Value = "Order ID";
+                worksheet.Cell(1, 3).Value = "Time";
+                worksheet.Cell(1, 4).Value = "Owner";
 
                 var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
                 var data = _orderService.GetAllOrdersByUser(userId);
@@ -86,14 +87,15 @@ namespace BookStore.Web.Controllers
                 for (int i = 0; i < data.Count(); i++)
                 {
                     var order = data[i];
-                    worksheet.Cell(i + 2, 1).Value = order.Id.ToString();
-                    worksheet.Cell(i + 2, 2).Value = order.OrderDate.Date.ToShortDateString() + ", " + order.OrderDate.ToShortTimeString();
-                    worksheet.Cell(i + 2, 3).Value = order.Owner.Email;
+                    worksheet.Cell(i + 2, 1).Value = (i+1).ToString();
+                    worksheet.Cell(i + 2, 2).Value = order.Id.ToString();
+                    worksheet.Cell(i + 2, 3).Value = order.OrderDate.Date.ToShortDateString() + ", " + order.OrderDate.ToShortTimeString();
+                    worksheet.Cell(i + 2, 4).Value = order.Owner.Email;
 
                     for (int j = 0; j < order.BooksInOrder.Count(); j++)
                     {
-                        worksheet.Cell(1, j + 4).Value = "Book " + (j + 1);
-                        worksheet.Cell(i + 2, j + 4).Value = "\"" + order.BooksInOrder.ElementAt(j).Book.Title + "\" by " + order.BooksInOrder.ElementAt(j).Book.Author.Name;
+                        worksheet.Cell(1, j + 5).Value = "Book " + (j + 1);
+                        worksheet.Cell(i + 2, j + 5).Value = "\"" + order.BooksInOrder.ElementAt(j).Book.Title + "\" by " + order.BooksInOrder.ElementAt(j).Book.Author.Name;
 
                     }
                 }
