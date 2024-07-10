9c855da4707d22ffbe237dbf3a13dc8fb69d189a
Nikola-Jankovikj <nikola.jankovikj@students.finki.ukim.mk>
2024-07-10 14:55:16 +0200

    Update OrdersController.cs

--- BookStore.Web/Controllers/OrdersController.cs
+++ BookStore.Web/Controllers/OrdersController.cs
@@ -39,65 +39,6 @@ namespace BookStore.Web.Controllers
             return View(order);
         }
 
-<<<<<<< HEAD
-        [HttpGet]
-        public FileContentResult CreateInvoice(Guid id)
-        {
-            var order = _orderService.GetDetailsForOrder(id);
-
-            string fileName = "Order" + order.OrderDate.ToShortDateString() + ".xlsx";
-            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-
-            using (var workBook = new XLWorkbook())
-            {
-                IXLWorksheet worksheet = workBook.Worksheets.Add("Order");
-
-                worksheet.Cell(1, 1).Value = "Order Export";
-
-                worksheet.Cell(2, 1).Value = "Order ID";
-                worksheet.Cell(2, 2).Value = order.Id;
-
-                worksheet.Cell(3, 1).Value = "Order Time";
-                worksheet.Cell(3, 2).Value = order.OrderDate.Date.ToShortDateString() + ", " + order.OrderDate.ToShortTimeString();
-
-                worksheet.Cell(4, 1).Value = "Owner";
-                worksheet.Cell(4, 2).Value = order.OwnerEmail;
-
-                worksheet.Cell(5, 1).Value = "Total Price ($)";
-
-                worksheet.Cell(7, 1).Value = "No.";
-                worksheet.Cell(7, 2).Value = "Book";
-                worksheet.Cell(7, 3).Value = "Individual Price ($)";
-                worksheet.Cell(7, 4).Value = "Quantity";
-
-                var books = order.Books;
-                var total = 0.0;
-                for (int i = 0; i < books.Count(); i++)
-                {
-                    worksheet.Cell(i + 8, 1).Value = i+1;
-                    worksheet.Cell(i + 8, 2).Value = "\"" + books[i].Book.Title + "\" by " + books[i].Book.Author.Name;
-                    worksheet.Cell(i + 8, 3).Value = books[i].Book.Price;
-                    worksheet.Cell(i + 8, 4).Value = books[i].Quantity;
-
-                    total += (books[i].Quantity * books[i].Book.Price);
-                }
-
-                worksheet.Cell(5, 2).Value = total;
-
-                worksheet.Columns().AdjustToContents();
-
-                using (var stream = new MemoryStream())
-                {
-                    workBook.SaveAs(stream);
-                    var content = stream.ToArray();
-                    return File(content, contentType, fileName);
-                }
-            }
-
-        }
-
-=======
->>>>>>> f5c7a60... removed single export
         [HttpGet]
         public FileContentResult ExportAllOrders()
         {
